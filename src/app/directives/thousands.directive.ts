146637a10e875b87e898e9953ed9ce6f74a809ce
Ye_Altman <ye.altman@gmail.com>
2019-11-11 18:27:15 +0200

    - directive  output - style

--- src/app/directives/thousands.directive.ts
+++ src/app/directives/thousands.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Output, EventEmitter } from '@angular/core';
 
 @Directive({
   selector: '[appThousands]'
@@ -12,13 +12,25 @@ export class ThousandsDirective {
     const input = this.element.nativeElement;
     const value = input.value;
 
+    if(!value) return;
+    
+    const valueNum = +value;
+
+    let newValue: number;
+
     if (value.startsWith("=")) {
-      let exactValue = value.substr(1);
-      input.value = exactValue;
+      newValue = +value.substr(1);
+    }
+    else if (valueNum <= 1000) {
+      newValue = valueNum * 1000;
     }
 
-    else if (+value < 1000) {
-      input.value = (+value * 1000).toString();
+    const newValueStr = newValue.toString();
+    if (newValueStr != value) {
+      input.value = newValueStr;
+      this.ngModelChange.emit(newValue);
     }
   }
+
+  @Output() ngModelChange: EventEmitter<number> = new EventEmitter<number>();
 }
